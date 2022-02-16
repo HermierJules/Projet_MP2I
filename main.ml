f771b3337a3d7756510a039eb9abf50efade606b
Gi-boss <gillianz@free.fr>
2022-02-16 20:20:28 +0100

    Bientot fini, les ennemis se deplacent mais il faut pouvoir tourner les attaques + les ennemis s'arrtent d'attaquer ?

--- main.ml
+++ main.ml
@@ -5,7 +5,8 @@ type skill = {
 	name : string;
 	description: string;
 	range : (int*int) list;
-	dmg : int
+	dmg : int;
+	cost : int
 	}
 type attaque = Nulle | Existe of skill
 
@@ -28,12 +29,11 @@ type tile = Bord | Mur | Vide | Allie of entite | Ennemi of entite
 
 
 (*definitions*)
+let dist x1 y1 x2 y2 =
+	let distance = Float.sqrt ((float_of_int ((x1-x2)*(x1-x2)))+.(float_of_int ((y1-y2)*(y1-y2)))) in
+	distance
 
 let pathfinder map x1 y1 x2 y2 =
-    let dist x1 y1 x2 y2 =
-        let distance = Float.sqrt ((float_of_int ((x1-x2)*(x1-x2)))+.(float_of_int ((y1-y2)*(y1-y2)))) in
-        distance
-    in
     let x = ref x1 and y = ref y1 in
 	let flag = ref (false,(0,0)) in
 	for i=1 to 10 do
@@ -55,7 +55,10 @@ let pathfinder map x1 y1 x2 y2 =
 	match !flag with
 	| (b,coords) -> if b = true then coords else (x1,y1) 
 
-
+let rec in_list x l =
+	match l with
+	| [] -> false
+	| t::q -> if t=x then true else in_list x q
 
 let cases = "##################################-------################-----------############---------------#########-----------------#######-------------------######-------------------#####---------------------####---------------------###-----------------------##-----------------------##-----------------------##-----------------------##-----------------------##-----------------------##-----------------------###---------------------####---------------------#####-------------------######-------------------#######-----------------#########---------------############-----------################-------##################################"
 
@@ -97,14 +100,16 @@ let blast_skill = {
 	name = "Explosion";
 	description = "Attaque autour du personnage (Portee: 2)";
 	range = [(-2,0);(-1,-1);(-1,0);(-1,1);(0,-2);(0,-1);(0,1);(0,2);(1,-1);(1,0);(1,1);(2,0)];
-	dmg = 3;
+	dmg = 4;
+	cost = 3;
 }
 
 let ray_skill = {
 	name = "Rayon";
 	description = "Attaque droit devant le personnage (Portee: 5)";
 	range = [(1,0);(2,0);(3,0);(4,0);(5,0)];
-	dmg = 4;
+	dmg = 5;
+	cost = 4;
 }
 
 let slash_skill = {
@@ -112,6 +117,7 @@ let slash_skill = {
 	description = "Coup devant le personnage (Portee: 1)";
 	range = [(1,-1);(1,0);(1,1)];
 	dmg = 6;
+	cost = 0;
 }
 
 let healAura_skill = {
@@ -119,22 +125,29 @@ let healAura_skill = {
 	description = "Soin autour du personnage (Portee: 2)";
 	range = [(-2,0);(-1,-1);(-1,0);(-1,1);(0,-2);(0,-1);(0,1);(0,2);(1,-1);(1,0);(1,1);(2,0)];
 	dmg = -4;
+	cost = 3;
 }
 
-let punch_skill = {
-	name = "Frappe";
-	description = "Coup puissant droit devant le personnage";
-	range = [(1,0)];
-	dmg = 4;
-}
-
-let a = {
-	hpmax = 15;
-	mpmax = 10;
-    hp = 15;
-    mp = 10;
+let mage = {
+	hpmax = 20;
+	mpmax = 20;
+    hp = 20;
+    mp = 20;
     x = 4;
 	y = 9;
+    skills= (Existe blast_skill,Existe ray_skill,Existe slash_skill,Existe healAura_skill);
+	moves = 0;
+	can_move = true;
+	can_attack = true;
+}
+
+let warrior = {
+	hpmax = 30;
+	mpmax = 10;
+    hp = 30;
+    mp = 10;
+    x = 4;
+	y = 15;
     skills= (Existe blast_skill,Existe ray_skill,Existe slash_skill,Existe healAura_skill);
 	moves = 0;
 	can_move = true;
@@ -146,10 +159,10 @@ let e1 = {
 	mpmax = 5;
     hp = 10;
     mp = 5;
-    x = 7;
+    x = 20;
 	y = 9;
-    skills= (Existe punch_skill, Nulle,Nulle,Nulle);
-	moves = 0;
+    skills= (Existe slash_skill,Existe blast_skill,Existe ray_skill,Existe healAura_skill);
+	moves = 3;
 	can_move = true;
 	can_attack = true;
 }
@@ -161,8 +174,21 @@ let e2 = {
     mp = 5;
     x = 20;
 	y = 15;
-    skills= (Existe punch_skill, Nulle,Nulle,Nulle);
-	moves = 0;
+    skills= (Existe slash_skill,Existe blast_skill,Existe ray_skill,Existe healAura_skill);
+	moves = 3;
+	can_move = true;
+	can_attack = true;
+}
+
+let e3 = {
+	hpmax = 10;
+	mpmax = 5;
+    hp = 10;
+    mp = 5;
+    x = 21;
+	y = 12;
+    skills= (Existe slash_skill,Existe blast_skill,Existe ray_skill,Existe healAura_skill);
+	moves = 3;
 	can_move = true;
 	can_attack = true;
 }
@@ -220,7 +246,7 @@ let paires =
 
 
 
-let couleur texte  fond =
+let couleur texte fond =
     attron (A.color_pair paires.(texte).(fond))
 
 
@@ -269,9 +295,10 @@ let draw_UI_main ent cursor =
     ignore (mvaddstr 5 30 (Printf.sprintf "HP: %d/%d" ent.hp ent.hpmax));
     ignore (mvaddstr 5 50 (Printf.sprintf "MP: %d/%d" ent.mp ent.mpmax));
     ignore (mvaddstr 10 35 (Printf.sprintf "Que faire ? :"));
-    if ent.can_move then ignore (mvaddstr 15 30 (Printf.sprintf "Se deplacer :    D"));
-    if ent.can_attack then ignore (mvaddstr 18 30 (Printf.sprintf "Attaquer :       A"));
-    ignore (mvaddstr 21 30 (Printf.sprintf "Finir le tour :  F"));
+    if ent.can_move then ignore (mvaddstr 15 30 (Printf.sprintf "D : Se deplacer"));
+    if ent.can_attack then ignore (mvaddstr 18 30 (Printf.sprintf "A : Attaquer"));
+    ignore (mvaddstr 21 30 (Printf.sprintf "S : Changer de personnage"));
+    ignore (mvaddstr 25 30 (Printf.sprintf "F : Finir le tour"));
     if cursor = 1 then putpixel rouge_clair 3 45 else putpixel rouge_clair 3 50
 
 
@@ -302,13 +329,22 @@ let draw_UI_Attaques ent =
 				draw_skill 35 30 d 'P'
 end
 	
+let unwrap_skill s =
+	match s with
+	| Existe x -> x
+	| Nulle -> {name = "";
+				description = "";
+				range = [];
+				dmg = 0;
+				cost = 0}
+	
 let move_entite map ent dx dy =
 	if ent.moves > 0 then
 		if map.(ent.y+dy).(ent.x+dx) = Vide then begin
 			ent.x <- ent.x + dx;
 			ent.y <- ent.y + dy;
 			ent.moves <- ent.moves - 1;
-			match map.(ent.y).(ent.x) with
+			match map.(ent.y-dy).(ent.x-dx) with
 			| Allie _ -> map.(ent.y).(ent.x) <- Allie ent; map.(ent.y-dy).(ent.x-dx) <- Vide;
 			| Ennemi _ -> map.(ent.y).(ent.x) <- Ennemi ent; map.(ent.y-dy).(ent.x-dx) <- Vide;
 			(* Les match suivants n'ont pas de sens mais produisent une erreur s'ils n'existent pas (pattern non vérifié) *)
@@ -335,10 +371,38 @@ let use_skill ent s map =
 					  | Allie a -> begin take_dmg a dmg map; use_skill_aux dmg q end
 					  | _ -> use_skill_aux dmg q;
 	in 
-	match s with
-	| Existe move -> use_skill_aux move.dmg move.range
-	| Nulle -> use_skill_aux 0 []
+	let move = unwrap_skill s in
+	use_skill_aux move.dmg move.range;
+	ent.mp <- ent.mp - move.cost
 
+let activate_skill ent skill_sel atk_ready map=
+	if !ent.mp >= (unwrap_skill (find_skill !ent !skill_sel)).cost then begin
+		use_skill !ent (find_skill !ent !skill_sel) map;
+		skill_sel := 0;
+		!ent.can_attack <- false;
+		atk_ready := false;
+	end
+	
+
+let enemies_turn enemies map =
+	let enemy_turn e target=
+		for i=1 to 3 do
+			e.moves <- e.moves + 1;
+			if i=i then 
+				match (pathfinder map e.x e.y target.x target.y) with
+				| (x,y) -> move_entite map e (x-e.x) (y-e.y)
+		done;
+		
+		match e.skills with
+		| (s1,s2,s3,s4) -> if in_list (target.x-e.x,target.y-e.y) (unwrap_skill s1).range && e.can_attack then activate_skill (ref e) (ref 1) (ref true) map;
+						   if in_list (target.x-e.x,target.y-e.y) (unwrap_skill s2).range && e.can_attack then activate_skill (ref e) (ref 2) (ref true) map;
+						   if in_list (target.x-e.x,target.y-e.y) (unwrap_skill s3).range && e.can_attack then activate_skill (ref e) (ref 3) (ref true) map;
+						   if in_list (target.x-e.x,target.y-e.y) (unwrap_skill s4).range && e.can_attack then activate_skill (ref e) (ref 4) (ref true) map
+	in
+	for i=0 to Array.length enemies -1 do
+		if (dist enemies.(i).x enemies.(i).y mage.x mage.y) < (dist enemies.(i).x enemies.(i).y warrior.x warrior.y) then enemy_turn enemies.(i) mage
+		else enemy_turn enemies.(i) warrior
+	done
 
 
 let _ =
@@ -350,12 +414,15 @@ Random.self_init ();
     let frames = ref 0 in
 
     let m = mapofstring cases in
-	m.(a.y).(a.x) <- Allie a;
+	m.(mage.y).(mage.x) <- Allie mage;
+	m.(warrior.y).(warrior.x) <- Allie warrior;
 	m.(e1.y).(e1.x) <- Ennemi e1;
 	m.(e2.y).(e2.x) <- Ennemi e2;
+	m.(e3.y).(e3.x) <- Ennemi e3;
 	generate_walls m;
-	let attack_ready = ref false in
-	let skill_selected = ref 0 in
+	let attack_ready = ref false and skill_selected = ref 0 in
+	let a = ref mage in
+	let all_enemies = [|e1;e2;e3|] in
     (* boucle principale *)
     while !continue do
         (* le clear permet de ne pas avoir de problèmes avec les animations
@@ -363,17 +430,18 @@ Random.self_init ();
            réecrire par dessus ce qui a changé *)
         clear ();
         couleur rouge noir;
-		m.(a.y).(a.x) <- Allie a;
+		
         draw_board m h;
+		putpixel bleu !a.x !a.y;
 		
 		couleur blanc noir;
 		if !attack_ready then begin 
-			draw_UI_Attaques a;
-			match (find_skill a !skill_selected) with
-			| Existe s -> draw_skill_range (a.x,a.y) s m;
+			draw_UI_Attaques !a;
+			match (find_skill !a !skill_selected) with
+			| Existe s -> draw_skill_range (!a.x,!a.y) s m;
 			| Nulle -> skill_selected := 0;
 		end
-		else if a.moves = 0 then draw_UI_main a 1;
+		else if !a.moves = 0 then draw_UI_main !a 1;
 
         incr frames;
 
@@ -390,52 +458,55 @@ Random.self_init ();
             (* attention certaines touches sont spéciales et ne
                peuvent pas être converties en caractère comme les
                touches fléchées *)
-            if c = Key.down then move_entite m a 0 1
-            else if c = Key.up then move_entite m a 0 (-1)
-            else if c = Key.left then move_entite m a (-1) 0 
-            else if c = Key.right then move_entite m a 1 0
+            if c = Key.down then move_entite m !a 0 1
+            else if c = Key.up then move_entite m !a 0 (-1)
+            else if c = Key.left then move_entite m !a (-1) 0 
+            else if c = Key.right then move_entite m !a 1 0
             else (match char_of_int c with
                 (* des caractères normaux *)
                 | 'q' -> continue := false
-				| 'd' -> if a.can_move && (not !attack_ready) then begin a.moves <- 5; a.can_move <- false end
-				| 'a' -> if a.moves=0 && a.can_attack then attack_ready := true;
+				| 'd' -> if !a.moves > 0 then begin 
+							!a.moves <- 0;
+							!a.can_move <- false
+						end
+						else if !a.can_move && (not !attack_ready) then begin 
+							!a.moves <- 5;
+							!a.can_move <- false 
+						end
+				| 'a' -> if !a.moves=0 && !a.can_attack then attack_ready := true;
 				| 'u' -> if !attack_ready then begin
-							if !skill_selected = 1 then begin
-								use_skill a (find_skill a !skill_selected) m;
-								skill_selected := 0;
-								a.can_attack <- false;
-								attack_ready := false;
-							end
+							if !skill_selected = 1 then activate_skill a skill_selected attack_ready m
 							else skill_selected := 1;
 						end
 				| 'i' -> if !attack_ready then begin
 							if !skill_selected = 2 then begin
-								use_skill a (find_skill a !skill_selected) m;
+								use_skill !a (find_skill !a !skill_selected) m;
 								skill_selected := 0;
-								a.can_attack <- false;
+								!a.can_attack <- false;
 								attack_ready := false;
 							end
 							else skill_selected := 2;
 						end
 				| 'o' -> if !attack_ready then begin
 							if !skill_selected = 3 then begin
-								use_skill a (find_skill a !skill_selected) m;
+								use_skill !a (find_skill !a !skill_selected) m;
 								skill_selected := 0;
-								a.can_attack <- false;
+								!a.can_attack <- false;
 								attack_ready := false;
 							end
 							else skill_selected := 3;
 						end
 				| 'p' -> if !attack_ready then begin
 							if !skill_selected = 4 then begin
-								use_skill a (find_skill a !skill_selected) m;
+								use_skill !a (find_skill !a !skill_selected) m;
 								skill_selected := 0;
-								a.can_attack <- false;
+								!a.can_attack <- false;
 								attack_ready := false;
 							end
 							else skill_selected := 4;
 						end
-				| 'f' -> a.can_move <- true;a.can_attack <- true; 
+				| 's' -> if !a = mage then a:=warrior else a:=mage;
+				| 'f' -> mage.can_move <- true; mage.can_attack <- true; warrior.can_move <- true; warrior.can_attack <- true; enemies_turn all_enemies m
                 | _ -> ())
         end
     done;
